a8b73867acbab0390eee127179ecb2fcdfbac339
cadenzah <cadenzah93@gmail.com>
2020-08-24 14:50:00 +0900

    added type for video.js, basic form of codes

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,31 @@
-console.log('Hello, video.js plugin!')
+import videojs from 'video.js';
+import initializeThumbnailSprite from './utils/initializeThumbnailSprite';
+
+const Plugin = videojs.getPlugin('plugin');
+
+class ThumbnailSprite extends Plugin {
+  private options: ThumbnailSprite.Options;
+
+  constructor(player: videojs.Player, options: ThumbnailSprite.Options) {
+    super(player);
+    this.options = options;
+
+    // When player instance is ready, initialize the plugin
+    this.player.ready(() => {
+      initializeThumbnailSprite(
+        this.player,
+        this.options,
+      )
+    })
+  }
+}
+
+videojs.registerPlugin('thumbnailSprite', ThumbnailSprite);
+
+namespace ThumbnailSprite {
+  export interface Options {
+    [key: string]: any;
+  }
+}
+
+export default ThumbnailSprite;
---
+++ lib/utils/initializeThumbnailSprite.ts
@@ -0,0 +1,10 @@
+import { initializeThumbnailSprite } from "./index"
+
+import videojs from 'video.js';
+import TS from '../index';
+
+function initializeThumbnailSprite(player: videojs.Player, options: TS.Options) {
+
+}
+
+export default initializeThumbnailSprite;
