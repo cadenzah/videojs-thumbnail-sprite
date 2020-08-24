6ee5eb90eaccc0b5e105d3da8929383ca8940c59
cadenzah <cadenzah93@gmail.com>
2020-08-25 01:10:06 +0900

    added warning in case of overlapping sections of thumbnails

--- lib/utils/initializeThumbnailSprite.ts
+++ lib/utils/initializeThumbnailSprite.ts
@@ -27,6 +27,22 @@ function initializeThumbnailSprite(player: videojs.Player, options: TS.Options):
       return 1;
   });
 
+  // 0. 썸네일 구간에 겹치는 것이 있는지 확인
+  // 겹치면, 경고 발생
+  // 겹치는 시간이 있는 시간을 알려준다 (최소시점, 최대시점)
+  for (let i = 0 ; i < sprites.length - 1 ; i++) {
+    const e1Start = sprites[i].start;
+    const e1End = e1Start + sprites[i].duration;
+    const e2Start = sprites[i + 1].start;
+    const e2End = e2Start + sprites[i + 1].duration;
+
+    if (e2Start < e1End) {
+      const overlapStart = e1Start;
+      const overlapEnd = Math.max(e1End, e2End);
+      console.warn(`Provided thumbnail sprites have overlapping sections among them: [${secondToTimeString(overlapStart)} ~ ${secondToTimeString(overlapEnd)}] Preview images may not match the corresponding point of time correctly.`);
+    }
+  }
+
   progressCtrl.on('mousemove', () => generatePreview(player, controls, sprites));
   progressCtrl.on('touchmove', () => generatePreview(player, controls, sprites));
   player.addClass('vjs-sprite-thumbnails');
@@ -132,6 +148,28 @@ function applyStyle(parent: HTMLElement, obj: any) {
     else
       tooltipStyle.removeProperty(key);
   })
+}
+
+function secondToTimeString(seconds: number): string {
+  let resultSecond = 0, resultMinute = 0, resultHour = 0;
+  // 00:00:00 포맷으로
+  if (seconds <= 0) {
+    return `00:00:00`;
+  } else if (seconds < 60) {
+    // less than a minute
+    return `00:00:${seconds}`;
+  } else if (seconds < 3600) {
+    // less than an hour
+    resultMinute = seconds / 60;
+    resultSecond = seconds % 60;
+    return `00:${resultMinute}:${resultSecond}`;
+  } else {
+    // more than an hour
+    resultHour = seconds / 3600;
+    resultMinute = (seconds % 3600) / 60;
+    resultSecond = ((seconds % 3600) % 60) / 60;
+    return `${resultHour}:${resultMinute}:${resultSecond}`;
+  }
 }
 
 interface IIndexableComponent extends videojs.Component {
